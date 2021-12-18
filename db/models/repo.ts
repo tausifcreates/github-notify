7d33d23b22d82162194829b1b6f976c2b5d61032
LittleTasteOfHeaven <littletasteofheaven@protonmail.com>
2021-12-19 05:10:29 +0600

    valid open graph data.

--- db/models/repo.ts
+++ db/models/repo.ts
@@ -1,15 +1,40 @@
 import mongoose from "mongoose";
 
 const repoSchema = new mongoose.Schema({
-	site: String,
-	url: String,
-	title: String,
-	image: {
-		url: String,
-		width: String,
-		height: String
+	site: {
+		type: String,
+		required: true
 	},
-	description: String
+	url: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	desc: {
+		type: String,
+		required: true
+	},
+	image: {
+		url: {
+			type: String,
+			required: true
+		},
+		width: {
+			type: String,
+			required: true
+		},
+		height: {
+			type: String,
+			required: true
+		}
+	},
+	favicon: {
+		type: String,
+		required: true
+	}
 });
 
 const Repo = mongoose.model('repos', repoSchema);
--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
+import ogs, { OpenGraphImage, OpenGraphProperties } from "open-graph-scraper";
 import { port, webhook_secret } from "./keys";
 import("./db/connect");
-import runOgs from "./utils/scrapper";
 import Repo from "./db/models/repo";
 
 const app = express();
@@ -27,15 +27,54 @@ app.post('/', (req, res) => {
 	const hmac = crypto.createHmac('sha256', webhook_secret);
 	const payload = JSON.stringify(req.body);
 	const digest = "sha256=" + hmac.update(payload).digest('hex');
+
 	(digest.length === sig.length) && crypto.timingSafeEqual(Buffer.from(digest), Buffer.from(sig)) ?
 		(async () => {
 			const isPrivate = req.body.repository.private;
 			if (isPrivate) return res.sendStatus(405);
 			const htmlUrl: string = req.body.repository.html_url;
-			console.log(req.body.repository.private);
 			try {
-				const result = await runOgs({ url: htmlUrl });
-				console.log("result: ", result);
+				const ogResult = await ogs({ url: htmlUrl });
+				if (!ogResult.error) {
+					console.log("no error");
+					const { result } = ogResult;
+					let ogImg: string | OpenGraphImage = result.ogImage!;
+					type ogImgPropsType = {
+						url: string,
+						width: string,
+						height: string;
+					};
+					let ogImgProps: ogImgPropsType;
+					if (typeof ogImg === "object") {
+						ogImgProps = ogImg;
+					} else {
+						res.status(400).json({ message: "No og image was sent." });
+					}
+
+					Repo.findOne({ 'site': result.ogUrl },
+						async (err: Error, qDoc: Object) => {
+							if (err) return console.log(err);
+							if (!qDoc) {
+								const repoDoc = new Repo({
+									site: result.ogSiteName,
+									url: result.ogUrl,
+									title: result.ogTitle,
+									desc: result.ogDescription,
+									image: {
+										url: ogImgProps.url,
+										width: ogImgProps.width,
+										height: ogImgProps.height
+									},
+									favicon: result.favicon
+								});
+								try {
+									await repoDoc.save();
+								} catch (error) {
+									console.error(error);
+								}
+							}
+						});
+				}
 			} catch (error) {
 				console.log("An error happened: ", error);
 			}
